d9023a77ae57a02b3f0aeeea739997dbb6576f05
bcassidy1 <bcassidy@bancroftschool.org>
2016-11-14 10:06:10 -0500

    finished messages

--- main.js
+++ main.js
@@ -1,4 +1,53 @@
 function check(){
 
+	var question1 = document.quiz.question1.value;
+	var question2 = document.quiz.question2.value;
+	var question3 = document.quiz.question3.value;
+	var question4 = document.quiz.question4.value;
+	var correct = 0;
+
+	if (question1 == "katy perry") {
+		correct++;
+	}
+
+	if (question2 == "drew bledsoe") {
+		correct++;
+	}
+
+	if (question3 == "broncos") {
+		correct++;
+	}
+
+	if (question4 == "patriots") {
+		correct++;
+	}
+
+	var messages = ["awesome job!", "almost there", "get your head in the game!", "you could do better"];
+
+	var range;
+
+	if (correct < 1) {
+		range = 3;
+	}
+
+	if (correct > 1 && correct < 3) {
+		range = 2;
+	}
+
+	if (correct > 2 && correct < 4) {
+		range = 1;
+	}
+
+	if (correct = 4) {
+		range = 0;
+	}
+
+
 	document.getElementById("after_submit").style.visibility = "visible";
+
+
+
+
+	document.getElementById("message").innerHTML = messages[range];
+	document.getElementById("number_correct").innerHTML = "you got " + correct + " correct.";
 }
